34d91eae3bc99bbb9aa5652be700001e7c76c518
Marissa Nur Aini <marissarissarissa19@gmail.com>
2017-05-26 13:01:55 +0700

    tugasakhircommit

---
+++ src/ramalanbintang/RamalanBintang.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramalanbintang;
+
+/**
+ *
+ * @author Marissa
+ */
+public class RamalanBintang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
